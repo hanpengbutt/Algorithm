57224959ac183318a36089f9f0a89e21570c19c2
hanpengbutt <89825051+hanpengbutt@users.noreply.github.com>
2024-09-25 13:54:58 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 128 ms, Memory: 41548 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -1,57 +1,38 @@
 import sys
 from collections import deque
 
+input = sys.stdin.readline
 N, K = map(int, input().split())
-vector = [lambda X: X - 1, lambda X: X + 1, lambda X: 2 * X]
-reverseVector = [lambda X: X + 1, lambda X: X - 1, lambda X: X / 2]
 visited = [0] * 100001
-paths = []
-
-
-def bfs(start):
-    q = deque()
-    q.append(start)
-    visited[start] = 1
-
-    while not visited[K]:
-        path = []
-        while q:
-            now = q.popleft()
-            for f in vector:
-                next = f(now)
-                if -1 < next < 100001 and not visited[next]:
-                    visited[next] = visited[now] + 1
-                    path.append(next)
-        paths.append(path)
-        for p in path:
-            q.append(p)
-
-
-    print(visited[K] - 1)
-    return paths
+path = [0] * 100001
+vector = [lambda X: X - 1, lambda X: X + 1, lambda X: 2 * X]
 
 
 def solution():
-    if N == K:
-        print(0)
-        print(K)
-        return
-
     result = []
-    paths = bfs(N)
-    paths.reverse()
+    q = deque()
+    q.append(N)
+    visited[N] = 1
+
+    while not visited[K]:
+        now = q.popleft()
+
+        for f in vector:
+            next = f(now)
+            if -1 < next < 100001 and not visited[next]:
+                q.append(next)
+                visited[next] = visited[now] + 1
+                path[next] = now
+
+    print(visited[K] - 1)
+
     now = K
-
-    for path in paths[1:]:
-        for f in reverseVector:
-            next = int(f(now))
-            if next in path:
-                result.append(next)
-                now = next
-
+    while now != N:
+        result.append(now)
+        now = path[now]
     result.reverse()
-    for p in [N] + result + [K]:
-        print(p, end=" ")
+
+    print(N, *result)
 
 
 solution()
