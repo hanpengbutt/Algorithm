250c7e2a3ddac7cd97a932d6da43ae0fd6a31c2f
hanpengbutt <89825051+hanpengbutt@users.noreply.github.com>
2024-10-01 18:25:54 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,15 @@
+N = int(input())
+dp = [0] * 10001
+
+
+def solution():
+    dp[0] = 1
+    dp[1] = 1
+
+    for i in range(2, N + 1):
+        dp[i] = dp[i - 1] + 2 * dp[i - 2]
+
+    print(dp[N] % 10007)
+
+
+solution()
