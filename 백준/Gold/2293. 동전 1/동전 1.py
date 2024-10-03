e9f38ac3d3a086e8211eeb332842ba71b84b7cdb
hanpengbutt <89825051+hanpengbutt@users.noreply.github.com>
2024-10-03 14:56:45 +0900

    [Gold IV] Title: 동전 1, Time: 96 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -6,15 +6,14 @@ coins = [int(input()) for _ in range(N)]
 dp = [0] * (K + 1)
 
 
+dp[0] = 1
 def solution():
     for coin in coins:
-        if coin <= K:
-            dp[coin] += 1
-        for i in range(1, K + 1):
-            if -1 < i - coin:
-                dp[i] += dp[i - coin]
+        for i in range(coin, K + 1):
+            dp[i] += dp[i - coin]
 
     print(dp[K])
+
 
 
 solution()
