61bee2696af7caddce0ca47f9ad2d273b08df0cf
hanpengbutt <89825051+hanpengbutt@users.noreply.github.com>
2024-10-29 21:47:01 +0900

    [D3] Title: 부분 수열의 합, Time: 115 ms, Memory: 47,460 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
@@ -3,17 +3,19 @@ T = int(input())
 
 def solution():
     N, K = map(int, input().split())
-    l = [0] * (2**N)
     A = list(map(int, input().split()))
+    l = [0] * (sum(A) + 1)
+    l[A[0]] = 1
+    maxIdx = A[0]
 
-    for i in range(N):
+    for i in range(1, len(A)):
         num = A[i]
-        for j in range(0, 2**N, 2 ** (i + 1)):
-            for k in range(2**i):
-                l[j + k] += num
-
-    return len([x for x in l if x == K])
-
+        for j in range(maxIdx, 0, -1):
+            l[j + num] += l[j]
+        l[num] += 1
+        maxIdx += num
+    
+    return l[K] if K <= sum(A) else 0
 
 for i in range(T):
     print(f"#{i + 1} {solution()}")
