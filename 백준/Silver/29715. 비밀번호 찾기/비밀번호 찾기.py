c458b057fc69eab18943a68b31a65675791a6457
hanpengbutt <89825051+hanpengbutt@users.noreply.github.com>
2024-11-23 17:15:15 +0900

    [Silver III] Title: 비밀번호 찾기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/29715. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver/29715. 비밀번호 찾기/비밀번호 찾기.py
@@ -1,24 +1,24 @@
 N, M = map(int, input().split())
 X, Y = map(int, input().split())
-x = 0
-y = 0
+count = 0
 
 for _ in range(M):
     a, b = map(int, input().split())
     if a != 0:
-        x += 1
-    else:
-        y += 1
+        count += 1
 
 result = 1
+for i in range(9 - M, 9 - N, -1):
+    result *= i
 
-if M == 0:
-    for i in range(9, 9 - N, -1):
+
+if M != 0:
+    for i in range(N - M, 0, -1):
+        result /= i
+
+    for i in range(N - count, 0, -1):
         result *= i
-else:
-    for i in range(y):
-        result *= (N - x - i)
-    for i in range(N - x - y):
-        result *= (9 - x - y - i)
-result = result * X + (int(result / 3) - (1 if result % 3 == 0 else 0)) * Y
+
+result = int(result * X + (int(result / 3) - (1 if result % 3 == 0 else 0)) * Y)
 print(result)
+
