857743b0b9cbacfe70ccacb9c5143bb01cd53eba
hanpengbutt <89825051+hanpengbutt@users.noreply.github.com>
2025-01-09 17:07:01 +0900

    [level 1] Title: 실패율, Time: 4.41 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,23 +1,15 @@
 function solution(N, stages) {
     const stage = new Array(N + 1).fill(0);
     stages.forEach(v => stage[v - 1] += 1);
-    stage.reverse();
-    let answer = [];
+
+    let fails = {};
+    let total = stage.reduce((acc, cur) => acc + cur);
     
-    let cur_sum = stage[0];
-    for(let i = 1; i < N + 1; i++) {
-        if(stage[i] !== 0) {
-            answer.push(stage[i] / (cur_sum + stage[i]));
-        } else {
-            answer.push(0);
-        }
-        cur_sum += stage[i]
+    for(let i = 0; i < N; i++) {
+        fails[i + 1] = stage[i] / total;
+        total -= stage[i];
     }
     
-    answer.reverse();
-    answer = answer.map((value, idx) => ({value, idx}));
-    answer.sort((a, b) => b.value - a.value);
-    answer = answer.map(e => (e.idx + 1));
-    
-    return answer;
+    return Object.entries(fails).sort((a, b) => b[1] - a[1]).map(v => +v[0]);
+        
 }
