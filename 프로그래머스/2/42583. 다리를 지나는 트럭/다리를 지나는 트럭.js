319833b493250fb0c135c479912b8148ccfe0ca9
hanpengbutt <89825051+hanpengbutt@users.noreply.github.com>
2025-02-06 00:04:59 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 7.12 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,72 +1,23 @@
-class Node {
-    constructor(weight) {
-        this.progress = 1;
-        this.weight = weight;
-        this.next = null;
-    }
-}
-
-class Queue {
-    constructor() {
-        this.front = null;
-        this.rear = null;
-    }
-    
-    enqueue(weight) {
-        const node = new Node(weight);
-        if(this.isEmpty()) {
-            this.front = node;
-        } else {
-            this.rear.next = node;
-        }
-        this.rear = node;
-    }
-    
-    dequeue() {
-        if(!this.isEmpty()) {
-            const node = this.front;
-            this.front = node.next;
-            if(node.next === null) this.rear = null;
-            return node.weight;
-        }
-    }
-    
-    isEmpty() {
-        return this.front === null;
-    }
-    
-    increaseProgress() {
-        let nowNode = this.front;
-        while(nowNode) {
-            nowNode.progress += 1;
-            nowNode = nowNode.next;
-        }
-    }
-}
-
 function solution(bridge_length, weight, truck_weights) {
-    const queue = new Queue();
-    let weightSum = 0;
-    let result = 0
-    
-    do {
-        result += 1
-        queue.increaseProgress();
-        
-        if(queue.front && queue.front.progress === bridge_length + 1) {
-            weightSum -= queue.dequeue();
-        }
-        
-        if(truck_weights.length > 0) {
-            const nowWeight = truck_weights[0];
-            if(weightSum + nowWeight <= weight) {
-                weightSum += nowWeight;
-                queue.enqueue(nowWeight);
-                truck_weights.shift();
-            }
-        }
-        
-    } while(!queue.isEmpty());
-    
-    return result;
+  const queue = []; // 다리 위 트럭의 정보를 담고있는 큐
+  let bridge_weight = (time = 0); // 다리 위 트럭 무게의 합, 시간
+
+  do {
+    if (queue[0] && queue[0].time === time) {
+      bridge_weight -= queue.shift().weight;
+    }
+
+    if (truck_weights[0] + bridge_weight <= weight) {
+      bridge_weight += truck_weights[0];
+      queue.push({
+        weight: truck_weights[0],
+        time: time + bridge_length,
+      });
+      truck_weights.shift();
+    }
+
+    time += 1;
+  } while (queue.length > 0);
+
+  return time;
 }
