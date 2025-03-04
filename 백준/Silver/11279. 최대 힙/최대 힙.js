9fa834b44a69e0439641cd980923ff7a6ab70e59
hanpengbutt <89825051+hanpengbutt@users.noreply.github.com>
2025-03-04 23:03:02 +0900

    [Silver II] Title: 최대 힙, Time: 280 ms, Memory: 40516 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.js
@@ -0,0 +1,87 @@
+let fs = require('fs');
+let [N, ...x] = fs
+  .readFileSync(0, 'utf8')
+  .trim()
+  .split('\n')
+  .map((v) => +v);
+
+function solution(N, x) {
+  class MaxHeap {
+    constructor() {
+      this.heap = [];
+    }
+
+    // 크기 반환
+    get size() {
+      return this.heap.length;
+    }
+
+    // 추가
+    add(value) {
+      this.heap.push(value);
+
+      let idx = this.size - 1;
+      let parentIdx = Math.floor((idx - 1) / 2);
+
+      while (parentIdx > -1 && this.heap[idx] >= this.heap[parentIdx]) {
+        this.swap(idx, parentIdx);
+        idx = parentIdx;
+        parentIdx = Math.floor((idx - 1) / 2);
+      }
+    }
+
+    // 삭제
+    delete() {
+      if (this.size === 0) {
+        return 0;
+      }
+
+      if (this.size === 1) {
+        return this.heap.pop();
+      }
+
+      const maxItem = this.heap[0];
+      this.heap[0] = this.heap.pop();
+
+      let idx = 0;
+      let leftIdx = idx * 2 + 1;
+      let rightIdx = idx * 2 + 2;
+
+      while (
+        (leftIdx < this.size && this.heap[leftIdx] >= this.heap[idx]) ||
+        (rightIdx < this.size && this.heap[rightIdx] >= this.heap[idx])
+      ) {
+        let childIdx = leftIdx;
+        if (rightIdx < this.size && this.heap[leftIdx] < this.heap[rightIdx]) {
+          childIdx = rightIdx;
+        }
+        this.swap(idx, childIdx);
+        idx = childIdx;
+        leftIdx = idx * 2 + 1;
+        rightIdx = idx * 2 + 2;
+      }
+
+      return maxItem;
+    }
+
+    // 교환
+    swap(idx1, idx2) {
+      [this.heap[idx1], this.heap[idx2]] = [this.heap[idx2], this.heap[idx1]];
+    }
+  }
+
+  const result = [];
+  const maxHeap = new MaxHeap();
+
+  x.forEach((v) => {
+    if (v > 0) {
+      maxHeap.add(v);
+    } else if (v === 0) {
+      result.push(maxHeap.delete());
+    }
+  });
+
+  return result.join('\n').trim();
+}
+
+console.log(solution(N, x));
