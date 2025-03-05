c347811ebae0fe5be34bdd3c0f72e3a8b3e58544
hanpengbutt <89825051+hanpengbutt@users.noreply.github.com>
2025-03-05 22:12:02 +0900

    [Gold V] Title: 두 용액, Time: 196 ms, Memory: 27704 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.js
@@ -0,0 +1,33 @@
+let fs = require('fs');
+let [N, s] = fs.readFileSync(0, 'utf8').trim().split('\n');
+
+N = +N;
+s = s.split(' ').map(Number);
+
+function solution(N, s) {
+  s.sort((a, b) => a - b);
+  let left = 0;
+  let right = N - 1;
+  let result = [s[left], s[right]];
+  let sum = Math.abs(s[left] + s[right]);
+
+  while (left < right) {
+    if (Math.abs(s[left] + s[right]) < sum) {
+        sum = Math.abs(s[left] + s[right]);
+        result = [s[left], s[right]];
+      }
+
+    if (s[left] + s[right] === 0) {
+      result = [s[left], s[right]];
+      break;
+    } else if (s[left] + s[right] > 0) {
+      right -= 1;
+    } else {
+      left += 1;
+    }
+  }
+
+  return result.join(' ');
+}
+
+console.log(solution(N, s));
