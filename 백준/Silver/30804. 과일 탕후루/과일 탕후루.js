58b76b12549f6235cac83e3c68a51608847b2f9d
hanpengbutt <89825051+hanpengbutt@users.noreply.github.com>
2025-03-05 16:26:32 +0900

    [Silver II] Title: 과일 탕후루, Time: 204 ms, Memory: 18888 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.js
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.js
@@ -4,43 +4,40 @@ let [N, S] = fs.readFileSync(0, 'utf8').trim().split('\n');
 N = +N;
 S = S.split(' ').map((v) => +v);
 
-function solution(N, S) {
+function solution(N, s) {
   let result = 0;
+  let left = (right = 0);
+  let slected = [s[0]];
+  let count = 1;
 
-  const fruits = [];
-  let fruit = S[0];
-  let count = 0;
-  for (let i = 0; i < N; i++) {
-    if (S[i] === fruit) {
-      count += 1;
-    } else {
-      fruits.push([fruit, count]);
-      fruit = S[i];
-      count = 1;
-    }
-
-    if (i === N - 1) fruits.push([fruit, count]);
-  }
-
-  let idx = 0;
-  let slected = [fruits[idx][0]];
-  count = fruits[idx][1];
-  while (idx < fruits.length - 1) {
-    idx += 1;
-    if (slected.includes(fruits[idx][0])) {
-      count += fruits[idx][1];
-    } else if (slected.length < 2) {
-      slected.push(fruits[idx][0]);
-      count += fruits[idx][1];
-    } else {
+  while (right < N) {
+    if (slected.length < 2) {
+      // 선택된 과일 종류가 2개보다 작은 경우, 다음 과일 선택
       if (count > result) result = count;
-      idx -= 1;
-      slected = [fruits[idx][0]];
-      count = fruits[idx][1];
+      right += 1;
+      if (!slected.includes(s[right])) {
+        slected.push(s[right]);
+      }
+      count += 1;
+    } else if (slected.length > 2) {
+      // 선택된 과일 종류가 2개보다 큰 경우
+      slected = [s[right], s[right - 1]];
+      left = right - 1;
+      count = 2;
+      while (s[left - 1] === s[right - 1]) {
+        left -= 1;
+        count += 1;
+      }
+    } else {
+      // 선택된 과일 종류가 2개인 경우
+      if (count > result) result = count;
+      right += 1;
+      if (!slected.includes(s[right])) {
+        slected.push(s[right]);
+      }
+      count += 1;
     }
   }
-
-  if (count > result) result = count;
 
   return result;
 }
