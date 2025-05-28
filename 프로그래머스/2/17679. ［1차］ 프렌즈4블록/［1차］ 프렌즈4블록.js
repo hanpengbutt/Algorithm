83b73896c8cff5f0ab2880312ef23c1a2b021804
hanpengbutt <89825051+hanpengbutt@users.noreply.github.com>
2025-05-28 14:05:46 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 67.47 ms, Memory: 38.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -0,0 +1,54 @@
+function solution(m, n, board) {
+    let result = 0;
+    board = board.map((row) => row.split(''));
+    const vector = [[0, 0], [0, 1], [1, 0], [1, 1]];
+    
+    function bomb() {
+        let bombCount = 0;
+        let newBoard = [];
+        board.forEach((row) => {
+            newBoard.push([...row]);
+        });
+            
+        for(let row = 0; row < m - 1; row += 1) {
+            for(let col = 0; col < n - 1; col += 1) {
+                const set = new Set();
+                vector.forEach(([dr, dc]) => {
+                    set.add(board[row + dr][col + dc]);
+                });
+                if(set.size === 1) {
+                    vector.forEach(([dr, dc]) => {
+                        if(newBoard[row + dr][col + dc]) {
+                            newBoard[row + dr][col + dc] = 0;
+                            bombCount++;
+                        }   
+                    });
+                }
+            }
+        }
+        
+        board = newBoard;
+        return bombCount;
+    }
+    
+    function drop() {
+        for(let col = 0; col < n; col++) {
+            const newCol = [];
+            for(let row = m - 1; row > -1; row--) {
+                if(board[row][col]) newCol.push(board[row][col])
+            }
+            for(let row = m - 1; row > -1; row--) {
+                board[row][col] = (m - 1 - row) < newCol.length ? newCol[m - 1 - row] : 0;
+            }
+        }
+    }
+    
+    while(1) {
+        const bombCount = bomb();
+        if(bombCount === 0) break;
+        result += bombCount;
+        drop();
+    }
+    
+    return result;
+}
