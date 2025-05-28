ad7217e586ae0dd9f0d2d72314b235282d0a8a34
hanpengbutt <89825051+hanpengbutt@users.noreply.github.com>
2025-05-28 19:08:40 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 39.29 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -1,27 +1,10 @@
 function solution(files) {
+  const regExp = /^(\D+)(\d{1,5})(.*)$/;
+    
   files = files.map((file) => {
-    let newFile;
-    let flag = 1;
-    let firstIdx = 0;
-    let lastIdx = file.length;
-    for (let i = 0; i < file.length; i++) {
-      if (file[i] !== ' ' && Number.isInteger(+file[i])) {
-        if (flag) {
-          flag = 0;
-          firstIdx = i;
-        }
-      } else if (!flag) {
-        lastIdx = i;
-        break;
-      }
-    }
-    newFile = [
-      file.slice(0, firstIdx),
-      file.slice(firstIdx, lastIdx),
-      file.slice(lastIdx),
-    ];
-    return newFile;
-  });
+      const [_, head, number, tail] = regExp.exec(file);
+      return [head, number, tail];
+  })
 
   files.sort((a, b) => {
     let v = a[0].toLowerCase().localeCompare(b[0].toLowerCase());
